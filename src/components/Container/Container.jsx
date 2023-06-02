902c4b5e10661b2b049615dccec3eba70d2e7992
toma570 <itextoma@gmail.com>
2023-06-03 02:30:52 +0600

    add

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({children}) => {
-    return <div className='max-w-[1140px]  mx-auto'>{children}</div>;
+    return <div className='max-w-[1200px]  mx-auto'>{children}</div>;
 };
 
 export default Container;
--- src/components/Share/NavBar/NavBar.jsx
+++ src/components/Share/NavBar/NavBar.jsx
@@ -46,27 +46,41 @@ const NavBar = () => {
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
 			>
-				<li>
+				<li className='group relative'>
 					<span className='nav-item'>DOCUMENTS</span>
+					<ul className='absolute top-[130%] bg-black  rounded-lg overflow-hidden py-0 max-h-[0px] group-hover:py-2 group-hover:max-h-[300px] duration-300'>
+						<li className='sub-nav-item'>THE GAME</li>
+						<li className='sub-nav-item'>TEME GAME</li>
+						<li className='sub-nav-item'>PROBLEM/SOLUTION</li>
+						<li className='sub-nav-item'>LORE/BLACKSTORY</li>
+						<li className='sub-nav-item'>FACTION SUMMARY</li>
+					</ul>
 				</li>
 			</NavLink>
 			<NavLink
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
 			>
-				<li>
+				<li className='group relative'>
 					<span className='nav-item'>GAME</span>
+					<ul className='absolute top-[130%] bg-black  rounded-lg overflow-hidden py-0 max-h-[0px] group-hover:py-2 group-hover:max-h-[300px] duration-300'>
+						<li className='sub-nav-item'>THE GAME</li>
+						<li className='sub-nav-item'>TEME GAME</li>
+						<li className='sub-nav-item'>PROBLEM/SOLUTION</li>
+						<li className='sub-nav-item'>LORE/BLACKSTORY</li>
+						<li className='sub-nav-item'>FACTION SUMMARY</li>
+					</ul>
 				</li>
 			</NavLink>
 			<NavLink
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
 			>
 				<li>
 					<span className='nav-item'>FAQ</span>
@@ -76,7 +90,24 @@ const NavBar = () => {
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
+			>
+				<li className='group relative'>
+					<span className='nav-item'>SOCIAL MEDIA</span>
+					<ul className='absolute top-[130%] bg-black  rounded-lg overflow-hidden py-0 max-h-[0px] group-hover:py-2 group-hover:max-h-[300px] duration-300'>
+						<li className='sub-nav-item'>THE GAME</li>
+						<li className='sub-nav-item'>TEME GAME</li>
+						<li className='sub-nav-item'>PROBLEM/SOLUTION</li>
+						<li className='sub-nav-item'>LORE/BLACKSTORY</li>
+						<li className='sub-nav-item'>FACTION SUMMARY</li>
+					</ul>
+				</li>
+			</NavLink>
+			<NavLink
+				to={"text-page"}
+				className={`${({ isActive }) => {
+					isActive ? "active " : "default";
+				}}  py-3 px-4 font-bold text-white `}
 			>
 				<li>
 					<span className='nav-item'>PLAY</span>
@@ -86,7 +117,7 @@ const NavBar = () => {
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
 			>
 				<li>
 					<span className='nav-item'>RANKING</span>
@@ -96,7 +127,7 @@ const NavBar = () => {
 				to={"text-page"}
 				className={`${({ isActive }) => {
 					isActive ? "active " : "default";
-				}}  py-3 px-6 font-bold text-white `}
+				}}  py-3 px-4 font-bold text-white `}
 			>
 				<li>
 					<span className='nav-item'>NEWS</span>
@@ -154,7 +185,7 @@ const NavBar = () => {
 								to={"text-page"}
 								className={`${({ isActive }) => {
 									isActive ? "active " : "default";
-								}}  py-3 px-6 font-bold  `}
+								}}  py-3 px-4 font-bold  `}
 							>
 								<li className='nav-item'>ABOUT</li>
 							</NavLink>
@@ -162,7 +193,7 @@ const NavBar = () => {
 								to={"text-page"}
 								className={`${({ isActive }) => {
 									isActive ? "active " : "default";
-								}}  py-3 px-6 font-bold  `}
+								}}  py-3 px-4 font-bold  `}
 							>
 								<li className='nav-item'>BUY NFTS</li>
 							</NavLink>
@@ -170,7 +201,7 @@ const NavBar = () => {
 								to={"text-page"}
 								className={`${({ isActive }) => {
 									isActive ? "active " : "default";
-								}}  py-3 px-6 font-bold  `}
+								}}  py-3 px-4 font-bold  `}
 							>
 								<li className='nav-item'>DOCUMENTS</li>
 							</NavLink>
@@ -178,7 +209,7 @@ const NavBar = () => {
 								to={"text-page"}
 								className={`${({ isActive }) => {
 									isActive ? "active " : "default";
-								}}  py-3 px-6 font-bold  `}
+								}}  py-3 px-4 font-bold  `}
 							>
 								<li className='nav-item'>GAME</li>
 							</NavLink>
@@ -186,7 +217,7 @@ const NavBar = () => {
 								to={"text-page"}
 								className={`${({ isActive }) => {
 									isActive ? "active " : "default";
-								}}  py-3 px-6 font-bold  `}
+								}}  py-3 px-4 font-bold  `}
 							>
 								<li className='nav-item'>FAQ</li>
 							</NavLink>
